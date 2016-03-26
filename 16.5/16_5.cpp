d6e69c51df205f1a0fdd799e20eb0b508247c9d3
长洲 <changzhou1026@126.com>
2016-03-26 15:25:43 +0800

    智能指针

---
+++ 16.5/16_5.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+#include<memory>
+class Report {
+private:
+	std::string str;
+public:
+	Report(const std::string s) :str(s) {
+		std::cout << "Objected created!\n";
+	}
+	~Report() { std::cout << "Objected delete\n"; }
+	void comment() const { std::cout << str << "\n"; }
+};
+int main() {
+	{
+		std::auto_ptr<Report> ps(new Report("using auto_ptr"));
+	    ps->comment();
+	}
+	{
+		std::shared_ptr<Report> ps(new Report("using shared_ptr"));
+		ps->comment();
+	}
+	{
+		std::unique_ptr<Report> ps(new Report("using unique_ptr"));
+		ps->comment();
+	}
+}
